fa46f8d246dac4004091030908e4ef48b58af0b9
Jonas-Ln <83479494+Jonas-Ln@users.noreply.github.com>
2023-01-31 02:27:12 +0100

    Update

--- NLP_Preprocessing_Classifiying_Sentiment.py
+++ NLP_Preprocessing_Classifiying_Sentiment.py
@@ -33,23 +33,26 @@ from nltk.classify import NaiveBayesClassifier,accuracy
 from nltk.metrics.scores import (f_measure,precision,recall)
 import tqdm
 import os
-
+cwd = os.getcwd()
+import numpy as np
+from Neural_Network import Neural_Network
+#from .Neural_Network import Neural_Network
 
 
 
 lemma = WordNetLemmatizer()
 
-cwd = os.getcwd()
+
 
 #Output Model & Fearues
-filename_Features = 'Word_Features.csv'
+filename_Features = '/Word_Features.txt'
 
-filename_NN = 'Jonas_Neural_Net.sav'
+filename_NN = '/Jonas_Neural_Net.sav'
 
-filename_NB = 'NaiveBayes.sav'
+filename_NB = '/NaiveBayes.sav'
 
 # Load Data:
-DATA_PATH = cwd + '/training_1600000_processed_noemoticon.csv'
+DATA_PATH = cwd + '/Training_Data/training_1600000_processed_noemoticon.csv'
 
 
 
@@ -93,88 +96,6 @@ class NLP_Transformer:
         return tweet
 
 
-import numpy as np
-
-
-# Input of x_train and test_data
-class Neural_Network:
-    def __init__(self):
-        np.random.seed(2)
-
-
-    def forward_propagation(self, W1, b1, W2, b2, X):
-        Z1 = W1.dot(X) + b1
-        A1 = self.ReLU(Z1)
-        Z2 = W2.dot(A1) + b2
-        A2 = self.softmax(Z2)
-        return Z1, A1, Z2, A2
-
-    def back_propagation(self, Z1, A1, Z2, A2, W1, W2, X, Y):
-        m = Y.size
-        #encode_y
-        one_h_endoding = np.zeros((Y.max()+1, m))
-        one_h_endoding[Y, np.arange(m)] = 1
-        #encode_y = np.zeros((Y.size, Y.max()+1))
-        #enocde_y[np.arange(Y.size),Y] = 1
-        dZ2 = 2 * (A2 - one_h_endoding)
-        dW2 = 1 / m * dZ2.dot(A1.T)
-        db2 = 1 / m * np.sum(dZ2,1)
-        dZ1 = W2.T.dot(dZ2) * self.derivative_ReLU(Z1)
-        dW1 = 1 / m * dZ1.dot(X.T)
-        db1 = 1 / m * np.sum(dZ1,1)
-        return dW1, db1, dW2, db2
-
-    def derivative_ReLU(self, Z):
-        return Z > 0
-
-    def ReLU(self, Z):
-        return np.maximum(0, Z)
-
-    def softmax(self, Z):
-        exp = np.exp(Z-np.max(Z))
-        return exp / exp.sum(axis=0)
-
-    def initialize_parameters(self):
-        W1 = np.random.normal(size = (10, 2000)) * np.sqrt(1./2000)
-        b1 = np.random.normal(size = (10, 1)) * np.sqrt(1./10)
-        W2 = np.random.normal(size = (2, 10)) * np.sqrt(1./12)
-        b2 = np.random.normal(size = (2, 1)) * np.sqrt(1./2000)
-        #print(W1, b1, W2, b2)
-        return W1, b1, W2, b2
-
-    def update_parameters(self, W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-        W1 = W1 - alpha * dW1
-        b1 = b1 - alpha * np.reshape(db1,(10,1))
-        W2 = W2 - alpha * dW2
-        b2 = b2 - alpha * np.reshape(db2,(2,1))
-
-        return W1, b1, W2, b2
-
-    def gradient_descent(self, X, Y, iterations, alpha):
-        size, m = X.shape
-        W1, b1, W2, b2 = self.initialize_parameters()
-        for i in range(iterations):
-            Z1, A1, Z2, A2 = self.forward_propagation(W1, b1, W2, b2, X)
-            dW1, db1, dW2, db2 = self.back_propagation(Z1, A1, Z2, A2,W1, W2, X, Y)
-            W1, b1, W2, b2 = self.update_parameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-            if i % 10 == 0:
-                print("Iteration: ", i)
-                print("Accuracy ", self.get_accuracy(self.get_prediction(A2), Y))
-                print(self.get_prediction(A2), Y)
-
-        return W1, b1, W2, b2
-
-    def get_prediction(self, A2):
-        return np.argmax(A2, 0)
-
-
-    def get_accuracy(self, prediction, Y):
-        return np.sum(prediction == Y) / Y.size
-
-    def make_a_prediction(self, X, W1, b1, W2, b2):
-        _,_,_, A2 = self.forward_propagation(W1,b1,W2,b2,X)
-        predictions = self.get_prediction(A2)
-        return predictions, self.softmax(A2)
     '''
     def test_prediction(self, index, W1, b1, W2, b2):
         datensatz = X_train[:, index, None]'''
@@ -183,7 +104,7 @@ def load_transform_df ():
     df = pd.read_csv(DATA_PATH,delimiter=',',encoding='ISO-8859-1')
 
     df.columns = ['Sentiment', 'id', 'date', 'query', 'user', 'text']
-    df = df[['Sentiment','text']].sample(n=6000, random_state=0)
+    df = df[['Sentiment','text']].sample(n=5000, random_state=0)
 
 # Wenn daten ungleich gebalanced evtl aufslpitten zu 50/50:
     #df_neg = df[df['Sentiment']== 0].sample(n=50, random_state=0)
@@ -218,7 +139,9 @@ def export_features(most_words,features):
         feature_array.append(1 if most_words[i] in features else 0)
     return feature_array
 
+
 def main():
+    feature_size = 2000
     x_train, x_test, y_train, y_test  = load_transform_df()
 
     x_train_new, y_train = feature_data(x_train, y_train)
@@ -226,8 +149,9 @@ def main():
     feature_extract = FreqDist(sum([word for word in x_train_new], []))
     #feature_extract = FreqDist(sum([word.split(' ') for word in x_train], []))
 
-    most_used_words = list(feature_extract)[:2000]
+    most_used_words = list(feature_extract)[:feature_size]
     #df_muw = pd.DataFrame(, columns= most_used_words)
+    feature_size = len(most_used_words)
     print(most_used_words)
 
     x_train_feature = [(export_features(most_used_words, data)) for data in x_train_new]
@@ -240,12 +164,12 @@ def main():
     y_test_NN = np.array(y_test)
 
 
-    W1, b1, W2, b2 = Neural_Network().gradient_descent(x_train_NN, y_train_NN, 1500, 0.2)
+    W1, b1, W2, b2 = Neural_Network(feature_size).gradient_descent(x_train_NN, y_train_NN, 1500, 0.2)
 
     # Test Prediction:
-    y_pred, y_pred_proba = Neural_Network().make_a_prediction(x_test_NN, W1, b1, W2, b2)
+    y_pred, y_pred_proba = Neural_Network(feature_size).make_a_prediction(x_test_NN, W1, b1, W2, b2)
 
-    accuracy = Neural_Network().get_accuracy(y_pred, y_test_NN)
+    accuracy = Neural_Network(feature_size).get_accuracy(y_pred, y_test_NN)
 
     print(accuracy)
     # Export test and train Data
@@ -265,6 +189,12 @@ def main():
     plt.plot([0, 1], [0, 1], "k--", label="chance level (AUC = 0.5)")
     plt.axis("square")
     plt.show()
+
+    W1, b1, W2, b2 save(filename_Features
+
+    filename_NN
+
+
 '''
     #Model for test
     model = MultinomialNB()
--- Neural_Network.py
+++ Neural_Network.py
@@ -1,1 +1,84 @@
 
+
+import numpy as np
+
+# Input of x_train and test_data
+class Neural_Network:
+    def __init__(self, feature_size):
+        self.feature_size = feature_size
+        np.random.seed(2)
+
+
+    def forward_propagation(self, W1, b1, W2, b2, X):
+        Z1 = W1.dot(X) + b1
+        A1 = self.ReLU(Z1)
+        Z2 = W2.dot(A1) + b2
+        A2 = self.softmax(Z2)
+        return Z1, A1, Z2, A2
+
+    def back_propagation(self, Z1, A1, Z2, A2, W1, W2, X, Y):
+        m = Y.size
+        #encode_y
+        one_h_endoding = np.zeros((Y.max()+1, m))
+        one_h_endoding[Y, np.arange(m)] = 1
+        #encode_y = np.zeros((Y.size, Y.max()+1))
+        #enocde_y[np.arange(Y.size),Y] = 1
+        dZ2 = 2 * (A2 - one_h_endoding)
+        dW2 = 1 / m * dZ2.dot(A1.T)
+        db2 = 1 / m * np.sum(dZ2,1)
+        dZ1 = W2.T.dot(dZ2) * self.derivative_ReLU(Z1)
+        dW1 = 1 / m * dZ1.dot(X.T)
+        db1 = 1 / m * np.sum(dZ1,1)
+        return dW1, db1, dW2, db2
+
+    def derivative_ReLU(self, Z):
+        return Z > 0
+
+    def ReLU(self, Z):
+        return np.maximum(0, Z)
+
+    def softmax(self, Z):
+        exp = np.exp(Z-np.max(Z))
+        return exp / exp.sum(axis=0)
+
+    def initialize_parameters(self):
+        W1 = np.random.normal(size = (10, self.feature_size)) * np.sqrt(1./self.feature_size)
+        b1 = np.random.normal(size = (10, 1)) * np.sqrt(1./10)
+        W2 = np.random.normal(size = (2, 10)) * np.sqrt(1./12)
+        b2 = np.random.normal(size = (2, 1)) * np.sqrt(1./self.feature_size)
+        #print(W1, b1, W2, b2)
+        return W1, b1, W2, b2
+
+    def update_parameters(self, W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+        W1 = W1 - alpha * dW1
+        b1 = b1 - alpha * np.reshape(db1,(10,1))
+        W2 = W2 - alpha * dW2
+        b2 = b2 - alpha * np.reshape(db2,(2,1))
+
+        return W1, b1, W2, b2
+
+    def gradient_descent(self, X, Y, iterations, alpha):
+        size, m = X.shape
+        W1, b1, W2, b2 = self.initialize_parameters()
+        for i in range(iterations):
+            Z1, A1, Z2, A2 = self.forward_propagation(W1, b1, W2, b2, X)
+            dW1, db1, dW2, db2 = self.back_propagation(Z1, A1, Z2, A2,W1, W2, X, Y)
+            W1, b1, W2, b2 = self.update_parameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+            if i % 10 == 0:
+                print("Iteration: ", i)
+                print("Accuracy ", self.get_accuracy(self.get_prediction(A2), Y))
+                print(self.get_prediction(A2), Y)
+
+        return W1, b1, W2, b2
+
+    def get_prediction(self, A2):
+        return np.argmax(A2, 0)
+
+
+    def get_accuracy(self, prediction, Y):
+        return np.sum(prediction == Y) / Y.size
+
+    def make_a_prediction(self, X, W1, b1, W2, b2):
+        _,_,_, A2 = self.forward_propagation(W1,b1,W2,b2,X)
+        predictions = self.get_prediction(A2)
+        return predictions, self.softmax(A2)
